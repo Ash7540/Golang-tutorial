f64b56a254b7c68e300b016a1f517bde9045cb9f
Ashwin Chavan <86465817+Ash7540@users.noreply.github.com>
2024-01-02 13:02:04 +0530

    Add files via upload

---
+++ datatype/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a bool = true        // boolean
+	var b int = 5            // integer
+	var c int = -6           // signed integer
+	var d uint = 7           // unsigned integer
+	var e float32 = 3.14     // float
+	var f float64 = 1.7e+308 // double
+	var g string = "abc"     // string
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println(g)
+}
